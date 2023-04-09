69d665a6185c6246fbb3531c5b8f85c2a33a2092
TunaObject <ObedientWorker@gmail.com>
2023-04-09 12:38:01 -0700

    api calls

--- webserver/server.js
+++ webserver/server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
+// process env import 
+require('dotenv').config();
+// openai api
+
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+const axios = require("axios");
+
 const PORT = process.env.PORT || 5000;
 
 // Serve static files
@@ -22,8 +36,10 @@ app.get('/', (req, res) => {
   });
 
 // Get the description parameter from the URL
-app.get('/description/:desc', (req, res) => {
+app.get('/description/:desc', async (req, res) => {
   const description = req.params.desc;
+
+  console.log(getGeneratedMei(description));
 
   fs.readFile(path.join(__dirname, 'MIDIPlayer.html'), 'utf-8', (err, data) => {
     if (err) {
@@ -32,11 +48,66 @@ app.get('/description/:desc', (req, res) => {
       return;
     }
 
+
+      
+
     const descriptionHtml = `<div id="description">${description}</div>`;
     const modifiedData = data.replace('</body>', descriptionHtml + '</body>');
     res.send(modifiedData);
   });
 });
+
+async function getGeneratedMei(description) {
+    
+    const prompt = `Could you generate sheet music using the XML MEI format, and it's just a D major chord? 
+    here's an example of the format I'd like (this is a C major chord example):
+    <?xml version="1.0" encoding="UTF-8"?>
+<mei xmlns="http://www.music-encoding.org/ns/mei">
+  <meiHead>
+    <fileDesc>
+      <titleStmt>
+        <title>C Major Chord</title>
+      </titleStmt>
+    </fileDesc>
+  </meiHead>
+  <music>
+    <body>
+      <mdiv>
+        <score>
+          <scoreDef>
+            <staffGrp>
+              <staffDef n="1" clef.shape="G" clef.line="2" />
+            </staffGrp>
+          </scoreDef>
+          <section>
+            <measure n="1">
+              <staff n="1">
+                <layer n="1">
+                  <chord dur="4">
+                    <note pname="c" oct="4" />
+                    <note pname="e" oct="4" />
+                    <note pname="g" oct="4" />
+                  </chord>
+                </layer>
+              </staff>
+            </measure>
+          </section>
+        </score>
+      </mdiv>
+    </body>
+  </music>
+</mei>`;
+    const response = await openai.createChatCompletion({
+      model: "gpt-4",
+      messages: [{ role: "user", content: prompt }],
+      //prompt: prompt,
+      //max_tokens: 10,
+    });
+    console.log(response.data.choices[0].message.content);
+    //console.log(response.data.choices[0].text);
+    
+    return "actually just hardcoded for now";
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
